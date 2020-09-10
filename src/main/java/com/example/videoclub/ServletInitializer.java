9fda6dbbce7ebc9e210f694509b14cb87a9388b2
Deborah Yambenu <37391936+debyamitsh@users.noreply.github.com>
2020-09-10 11:53:16 +0200

    First commit

---
+++ src/main/java/com/example/videoclub/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.videoclub;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VideoclubApplication.class);
+    }
+
+}

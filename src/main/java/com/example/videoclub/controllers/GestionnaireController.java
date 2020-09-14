16013a7bb1bf9814cf95a938ff5568710f3f3c7b
savalone47 <savalonescott47@gmail.com>
2020-09-14 13:21:24 +0200

    updating controller

--- src/main/java/com/example/videoclub/controllers/GestionnaireController.java
+++ src/main/java/com/example/videoclub/controllers/GestionnaireController.java
@@ -1,4 +1,35 @@
 package com.example.videoclub.controllers;
 
+import com.example.videoclub.Models.entities.Gestionnaire;
+import com.example.videoclub.Models.repositories.GestionnaireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path="/gestion", value = "/gestion/add", method = RequestMethod.POST)
 public class GestionnaireController {
+    @Autowired
+    private GestionnaireRepository gestionnaireRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody
+    void addNewGestionnaire(@RequestParam int id,
+                        @RequestParam String nom,
+                        @RequestParam String prenom,
+                            @RequestParam String phone,
+                            @RequestParam String address){
+
+       Gestionnaire gestionnaire = new Gestionnaire();
+        gestionnaire.setId(id);
+        gestionnaire.setNom(nom);
+        gestionnaire.setPrenom(prenom);
+        gestionnaire.setPhone(phone);
+        gestionnaire.setAddress(address);
+    }
+    @GetMapping(path = "all")
+    public @ResponseBody
+    Iterable<Gestionnaire> getAllCassette(){
+        return gestionnaireRepository.findAll();
+    }
 }

24dfe6d537ea05f2694b257a167a4994845d8048
savalone47 <savalonescott47@gmail.com>
2020-09-14 13:16:14 +0200

    add cassette controller

--- src/main/java/com/example/videoclub/controllers/CassetteControlelr.java
+++ src/main/java/com/example/videoclub/controllers/CassetteControlelr.java
@@ -1,4 +1,33 @@
 package com.example.videoclub.controllers;
 
+import com.example.videoclub.Models.entities.Cassette;
+import com.example.videoclub.Models.repositories.CasseteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path="/cassette", value = "/cassette/add", method = RequestMethod.POST)
 public class CassetteControlelr {
+    @Autowired
+    private CasseteRepository casseteRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody
+    void addNewCassette(@RequestParam int id,
+                      @RequestParam String nom,
+                      @RequestParam String numero){
+        Cassette cassette = new Cassette();
+        cassette.setId(id);
+        cassette.setNom(nom);
+        cassette.setNumero(numero);
+    }
+    @GetMapping(path = "all")
+    public @ResponseBody
+    Iterable<Cassette> getAllCassette(){
+        return casseteRepository.findAll();
+    }
 }
+
+
+
